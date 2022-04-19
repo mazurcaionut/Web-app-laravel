210f0b428d6ccf59ff6376b9bbc9726d8d9d581d
Mazi <mazurcaionut99@gmail.com>
2022-04-19 22:44:05 +0100

    Created the Notification Model and also created the relationship with the Comment and User models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use \App\Models\User;
+use \App\Models\Notification;
+use \App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +12,7 @@ class Comment extends Model
 {
     use HasFactory;
 
+    public $with = ["comments", "user"];
 
     public function user() {
         return $this->belongsTo(User::class);
@@ -19,11 +22,23 @@ class Comment extends Model
         return $this->morphMany(Comment::class, 'commentable');
     }
 
+    public function post() {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function notification() {
+        return $this->morphOne(Notification::class, "notifiable");
+    }
 
     public function delete() {
         foreach($this->comments as $comment) {
             $comment->delete();
         }
+
+        if($this->notification) {
+            $this->notification->delete();
+        }
+
 
         return parent::delete();
     }
