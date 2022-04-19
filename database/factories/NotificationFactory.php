701069a1170282b51c41ec9eeea0c9b553adc835
Mazi <mazurcaionut99@gmail.com>
2022-04-19 22:48:04 +0100

    Created the seeders and factory for the notification model

---
+++ database/factories/NotificationFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Notification>
+ */
+class NotificationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'content' => $this->faker->
+                sentence($nbWords = rand($min = 1, $max = 12), $variableNbWords = true),
+            //
+        ];
+    }
+}
